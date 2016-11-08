7fba4ae1734042a26fedc2dee4f19266ddf08f5e
UserPL022Pc12 <UserPL022Pc12@SLFRPL022PC12.ensino.atec.local>
2016-11-08 11:09:00 +0000

    teste 08-11-16

---
+++ src/IndieGame/SensibleDummy.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IndieGame;
+
+/**
+ *
+ * @author UserPL022Pc12
+ */
+public abstract class SensibleDummy  {
+    String nome,cor;
+    
+    public void move(int x,int y){
+        
+    }
+}
---
+++ src/SuporteAulas/ProvaOral.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SuporteAulas;
+
+/**
+ *
+ * @author UserPL022Pc12
+ */
+public class ProvaOral {
+    private int fluencia;
+    int dominioMateria;
+
+    public int getFluencia() {
+        return fluencia;
+    }
+
+    public void setFluencia(int novaFluencia) {
+        this.fluencia = novaFluencia;
+    }
+    
+    
+}
